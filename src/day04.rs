7ae667d09c928e5ce85ce6559c902b098c1e7652
WilcodenBesten <whdenbesten@gmail.com>
2023-12-04 14:24:27 +0100

    Day 04 part two

--- src/day04.rs
+++ src/day04.rs
@@ -8,19 +8,39 @@ pub fn day04() -> std::io::Result<()> {
     let reader = BufReader::new(file);
 
     let mut answer_one: u32 = 0;
-    let answer_two: i32 = 0;
+    let mut answer_two: u32 = 0;
+
+    // nr cards, nr winning numbers
+    let mut cards: Vec<(u32, u32)> = Vec::new();
 
     for (_index, line) in reader.lines().enumerate() {
         let actual_line = line.unwrap();
 
         answer_one += get_points(&actual_line);
+
+        // Calculate this new card copies by traversing the cards list
+        // and adding up all copies of existing cards, subtracting the number of wins remaining by one
+        // and removing any entries that have no more wins remaining
+        let mut nr_cards_copies = 1;
+        cards.retain_mut(|(c, n)| {
+            nr_cards_copies += *c;
+            *n -= 1;
+            *n > 0
+        });
+
+        answer_two += nr_cards_copies;
+
+        let nr_matching_numbers = get_nr_matching_numbers(&actual_line);
+        if nr_matching_numbers > 0 {
+            cards.push((nr_cards_copies, nr_matching_numbers));
+        }
     }
 
     println!("Answer 1: {}, 2: {}", answer_one, answer_two);
     Ok(())
 }
 
-fn get_points(input: &str) -> u32 {
+fn get_nr_matching_numbers(input: &str) -> u32 {
     let start_winning_numbers = input.find(':').unwrap();
     let start_our_numbers = input.find('|').unwrap();
 
@@ -30,15 +50,11 @@ fn get_points(input: &str) -> u32 {
         .map(|x| x.to_string())
         .collect::<Vec<String>>();
 
-    println!("Win: {:?}", winning_numbers);
-
     let our_numbers = &input[start_our_numbers + 1..]
         .trim()
         .split(' ')
         .map(|x| x.to_string())
         .collect::<Vec<String>>();
-
-    println!("Our: {:?}", our_numbers);
 
     let mut sum: u32 = 0;
     for number in our_numbers {
@@ -48,26 +64,37 @@ fn get_points(input: &str) -> u32 {
 
         for winning_number in winning_numbers {
             if number == winning_number {
-                println!("{} is winning!", number);
                 sum += 1;
                 break;
             }
         }
     }
 
-    println!("sum {}", sum);
-
-    if sum > 0 {
-        sum = u32::pow(2, sum - 1);
-    }
-
     return sum;
+}
+
+fn get_points(input: &str) -> u32 {
+    let mut points: u32 = get_nr_matching_numbers(input);
+    if points > 0 {
+        points = u32::pow(2, points - 1);
+    }
+    return points;
 }
 
 #[rustfmt::skip]
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_get_nr_matching_numbers() {
+        assert_eq!(get_nr_matching_numbers("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"), 4);
+        assert_eq!(get_nr_matching_numbers("Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19"), 2);
+        assert_eq!(get_nr_matching_numbers("Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1"), 2);
+        assert_eq!(get_nr_matching_numbers("Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83"), 1);
+        assert_eq!(get_nr_matching_numbers("Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36"), 0);
+        assert_eq!(get_nr_matching_numbers("Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"), 0);
+    }
 
     #[test]
     fn test_get_points() {
