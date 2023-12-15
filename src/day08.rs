7fcc1f2b16c94ad30f2d0f635d9cf45dfb68e1b1
WilcodenBesten <whdenbesten@gmail.com>
2023-12-15 10:15:35 +0100

    Day 08 part two

--- src/day08.rs
+++ src/day08.rs
@@ -1,3 +1,4 @@
+use num::integer::lcm;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -8,7 +9,7 @@ pub fn day08() -> std::io::Result<()> {
     let reader = BufReader::new(file);
 
     let mut answer_one: usize = 0;
-    let mut answer_two: usize = 0;
+    let mut answer_two: u128 = 0;
 
     let mut directions: String = "".to_owned();
     let mut elements: Vec<(String, Node)> = Vec::with_capacity(1000);
@@ -26,28 +27,61 @@ pub fn day08() -> std::io::Result<()> {
     }
 
     let zzz = "ZZZ".to_string();
-    let mut current_element = "AAA".to_owned();
-    
+    let mut part_one_element = "AAA".to_owned();
+    let mut part_two_current_elements: Vec<(u128, String)> = Vec::with_capacity(1000);
+
+    for element in &elements {
+        if element.0.ends_with('A') {
+            part_two_current_elements.push((0, element.0.to_owned()));
+        }
+    }
+
     for direction in directions.chars().cycle() {
-        if current_element == zzz {
+        if part_one_element != zzz {
+            part_one_element = get_next_element(&elements, direction, &part_one_element);
+            answer_one += 1;
+        }
+
+        for part_two_element in part_two_current_elements.iter_mut() {
+            if part_two_element.1.ends_with('Z') {
+                continue;
+            }
+
+            part_two_element.1 = get_next_element(&elements, direction, &part_two_element.1);
+            part_two_element.0 += 1;
+        }
+
+        if part_two_current_elements.iter().all(|x| x.1.ends_with('Z')) {
             break;
         }
+    }
 
-        let result = elements
-            .iter()
-            .enumerate()
-            .find(|&(_, (string, _))| string == &current_element);
-        if direction == 'R' {
-            current_element = elements.get(result.unwrap().0).unwrap().1.right.to_owned();
+    for (i, part_two_element) in part_two_current_elements.iter().enumerate() {
+        if i == 0 {
+            answer_two = part_two_element.0;
         } else {
-            current_element = elements.get(result.unwrap().0).unwrap().1.left.to_owned();
+            answer_two = lcm(answer_two, part_two_element.0);
         }
-
-        answer_one += 1;
     }
 
     println!("Answer 1: {}, 2: {}", answer_one, answer_two);
     Ok(())
+}
+
+fn get_next_element(
+    elements: &Vec<(String, Node)>,
+    direction: char,
+    current_element: &str,
+) -> String {
+    let result = elements
+        .iter()
+        .enumerate()
+        .find(|&(_, (string, _))| string == &current_element);
+    if direction == 'R' {
+        return elements.get(result.unwrap().0).unwrap().1.right.to_owned();
+    } else {
+        return elements.get(result.unwrap().0).unwrap().1.left.to_owned();
+    }
 }
 
 #[derive(PartialEq, Debug)]
